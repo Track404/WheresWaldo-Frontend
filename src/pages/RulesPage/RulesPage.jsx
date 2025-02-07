52c38ed6ae1a2d82b82ea6401e0c0c489b90628e
Track404 <111713000+Track404@users.noreply.github.com>
2025-02-07 21:29:14 +0100

    Creation of rules page

--- src/pages/RulesPage/RulesPage.jsx
+++ src/pages/RulesPage/RulesPage.jsx
@@ -27,9 +27,63 @@ function RulesPage() {
     <>
       <Navbar />
       <div className={changeBackground()}>
-        <h1 className={styles.homepageText}>Rules Page</h1>
+        <h1 className={styles.homepageText}>Rules </h1>
 
-        <div className={styles.card}></div>
+        <div className={styles.card}>
+          <h2>Objective:</h2>
+          <p>
+            The objective of <strong>Where is Waldo?</strong> is to find Waldo,
+            a character wearing a red-and-white striped shirt, blue pants, and
+            glasses, hidden somewhere in a detailed, crowded illustration. Each
+            page of the book or game contains a scene filled with numerous
+            characters and objects, and Waldo is typically tucked somewhere in
+            the midst of the chaos. The goal is to spot Waldo as quickly as
+            possible.
+          </p>
+
+          <h2>Basic Rules:</h2>
+          <ul>
+            <li>
+              <span>Starting the Game:</span> Begin with a page or scene that
+              contains Waldo (and possibly other hidden characters, objects, or
+              items).
+            </li>
+            <li>
+              <span>Looking for Waldo:</span> Scan the image carefully. The
+              images are full of intricate details, and Waldo will always be in
+              the image but cleverly hidden.
+            </li>
+
+            <li>
+              <span>Game Levels:</span> Difficulty increases as the scenes
+              become more complex. More people, objects, and distractions make
+              it harder to spot Waldo.
+            </li>
+
+            <li>
+              <span>Winning the Game:</span> The goal is to find all the hidden
+              items (including Waldo) within a single round. Once you find them
+              all, the time you took to find them will be recorded. The faster
+              you complete it, the better your time.
+            </li>
+          </ul>
+
+          <h2>Tips for Finding Waldo:</h2>
+          <ul>
+            <li>
+              Start by identifying familiar features like Waldo is red-and-white
+              striped shirt, glasses, and blue pants.
+            </li>
+            <li>
+              Focus on smaller sections of the scene rather than scanning the
+              entire image at once.
+            </li>
+            <li>
+              Look for Waldo’s characteristic pose, as he stands out from the
+              rest of the crowd.
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
